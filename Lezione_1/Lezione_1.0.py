8acabd0c9b0853aa639fee1ee59fc75c476ceceb
alerossi <alessandrorossi131008@gmail.com>
2025-11-06 11:28:42 +0100

    Update Lezione_1.0.py

--- Lezione_1/Lezione_1.0.py
+++ Lezione_1/Lezione_1.0.py
@@ -12,7 +12,7 @@ GAME_TITLE = "Salva la Principessa - Lezione 1"
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(GAME_TITLE)
 
-# Carica lo sfondo
+# Carica lo sfondo, Mario e i blocchi
 background = pygame.image.load("sfondo.png").convert()
 mario_img = pygame.image.load("mario.png").convert_alpha()
 block_img = pygame.image.load("blocco.png").convert_alpha()
@@ -22,9 +22,6 @@ background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 mario_img = pygame.transform.scale(mario_img, (60, 60))
 block_width, block_height = 150, 40
 block_img = pygame.transform.scale(block_img, (block_width, block_height))
-
-
-background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
 # Clock per controllare il framerate
 clock = pygame.time.Clock()
